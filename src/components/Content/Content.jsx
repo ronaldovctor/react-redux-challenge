bf2092767ec992437e74b6a395cf408ebc50a5f5
Ronaldo <ronaldovctor@outlook.com>
2022-10-21 10:41:37 -0300

    login style

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Login } from '../Login/Login'
 
 export function Content() {
-	return <div>Content</div>
+	return (
+		<section className={`content max-width`}>
+			<Login />
+		</section>
+	)
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Login.module.scss'
 
 export function Login() {
 	function handleSubmit(event) {
